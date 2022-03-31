d5ef93148161dc9c988066c0e64cdd72693a6ead
phanixx <peta.stanek16@icloud.com>
2022-03-31 15:22:14 +0200

    add swiper + showcase

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,9 +5,28 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-    
+    private Nette\Database\Explorer $database;
+
+    private $slides;
+
+    public function __construct(Nette\Database\Explorer $database)
+    {
+        $this->database = $database;
+    }
+
+    public function actionDefault(): void 
+    {
+        $this->slides = $this->database->table('slides');
+       
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->slides = $this->slides;
+    }
 }
--- app/assets/entry.js
+++ app/assets/entry.js
@@ -2,5 +2,24 @@ import './sass/main.scss';
 import netteForms from 'nette-forms';
 import 'bootstrap';
 
+import Swiper, { Navigation, Pagination} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.mySwiper', {
+    modules: [Navigation, Pagination],
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
 window.Nette = netteForms;
 netteForms.initOnLoad();
