aff862ab1807d5c28dce6c38d67ae4085651f4b4
phanixx <peta.stanek16@icloud.com>
2022-03-31 18:58:21 +0200

    form

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -33,6 +33,34 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
         $this->template->slides = $this->slides;
         $this->template->pages = $this->pages;
         $this->template->references = $this->references;
+    }
+
+    protected function createComponentCtaForm(): Form
+    {
+        $ctaForm = new Form();
+        $ctaForm->addText('name', 'Jméno')->setRequired();
+        $ctaForm->addEmail('email', 'Email')->setRequired();
+        $ctaForm->addText('phone', 'Telefon')
+        ->setHtmlType('tel')
+        ->setEmptyValue('+420')
+        ->setRequired();
+        $ctaForm->addTextArea('message', 'Zpráva')
+        ->addRule($ctaForm::MAX_LENGTH, 'Poznámka je příliš dlouhá', 500)
+        ->setRequired();
+        $ctaForm->addSubmit('send',  'Poslat')->setHtmlAttribute('class', 'btn btn-primary');
         
+        $ctaForm->onSuccess[] = [$this, 'ctaProcess'];
+        
+        return $ctaForm;
+    }
+
+    public function ctaProcess($form, array $values): void
+    {
+        try{
+            $this->database->table('call_to_action')->insert($values);
+
+        } catch (AnyModelException $e){
+            $form->addError('Error');
+        }
     }
 }
