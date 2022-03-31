3d99db61093df46149e11ba08f2a26787f586bb8
phanixx <peta.stanek16@icloud.com>
2022-03-31 09:42:21 +0200

    layout + webpack.cfg

---
+++ app/assets/entry.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+import netteForms from 'nette-forms';
+import 'bootstrap';
+
+window.Nette = netteForms;
+netteForms.initOnLoad();
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { resolve: resolvePath } = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+const ROOT_PATH = __dirname;
+
+module.exports = {
+    mode: devMode ? 'development' : 'production',
+    entry: {
+        web: path.join(ROOT_PATH, "app/assets/entry.js"),
+    },
+    output: {
+        path: path.join(ROOT_PATH, "web/dist"),
+        publicPath: "/dist/",
+        filename: '[name].bundle.js',
+        assetModuleFilename: 'images/[name][ext][query]'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]ss|css)$/i,
+                use: [
+                    // fallback to style-loader in development
+                    devMode
+                        ? "style-loader"
+                        : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: "file-loader",
+                options: {
+                    outputPath: "fonts",
+                }
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].bundle.css",
+            chunkFilename: "[id].bundle.css",
+        }),
+        new webpack.ProvidePlugin({
+            naja: ['naja', 'default'],
+            Popper: ["popper.js", "default"]
+        })
+    ]
+};
