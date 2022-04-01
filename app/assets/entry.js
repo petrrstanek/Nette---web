6272be0cc8b33f3f1cdf8917cacf998a0ea8020e
phanixx <peta.stanek16@icloud.com>
2022-04-01 10:50:22 +0200

    swiper autoplay

--- app/assets/entry.js
+++ app/assets/entry.js
@@ -1,14 +1,18 @@
 import './sass/main.scss';
 import netteForms from 'nette-forms';
-import 'bootstrap';
+// import 'bootstrap';
 
-import Swiper, { Navigation, Pagination} from 'swiper';
+
+
+import Swiper, { Navigation, Pagination, Autoplay} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
 
 const swiper = new Swiper('.mySwiper', {
-    modules: [Navigation, Pagination],
+    modules: [Navigation, Pagination, Autoplay],
     cssMode: true,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -16,23 +20,31 @@ const swiper = new Swiper('.mySwiper', {
     },
     pagination: {
         el: ".swiper-pagination",
+        dynamicBullets: true,
     },
+    autoplay: {
+        delay: 4000,
+    },
+    loop: true,
     mousewheel: true,
     keyboard: true,
 });
 
 const refSwiper = new Swiper('.ref-swiper', {
-    modules: [ Navigation, Pagination ],
+    modules: [Pagination, Autoplay ],
     slidesPerView: 3,
     spaceBetween: 30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper.button-prev"
-    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
+    autoplay: {
+        delay: 4000,
+    },
+    loop: true,
+    mousewheel: true,
+    keyboard: true
 })
 
 window.Nette = netteForms;
